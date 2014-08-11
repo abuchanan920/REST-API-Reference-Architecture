7c8b116b83ff45c95752a5bcb372b3adb6080520
Andrew W. Buchanan <buchanan@difference.com>
2014-08-11 18:02:46 -0400

    More thorough cleaning of cluster

--- sbin/killAllContainers.sh
+++ sbin/killAllContainers.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for CORE in `seq 101 103`; do
 	DOCKER_HOST=172.17.8.$CORE:4243
-	for CONTAINER in `docker ps | cut -f 1 -d " " | grep -v CONTAINER`; do
+	for CONTAINER in `DOCKER_HOST=$DOCKER_HOST docker ps -a| cut -f 1 -d " " | grep -v CONTAINER`; do
+		echo "Stopping $CONTAINER... on $DOCKER_HOST"
 		docker stop $CONTAINER
 		docker rm $CONTAINER
 	done
