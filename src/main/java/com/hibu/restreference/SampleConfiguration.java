4086ab7bb5d41824432700443c4d404fbb3da3d1
Andrew W. Buchanan <buchanan@difference.com>
2014-08-11 18:05:29 -0400

    Created etcd backed SampleRepository

--- src/main/java/com/hibu/restreference/SampleConfiguration.java
+++ src/main/java/com/hibu/restreference/SampleConfiguration.java
@@ -15,11 +15,14 @@ public class SampleConfiguration extends EtcdConfiguration {
 	private static final String BASE_PATH = "/restreference";
 	private static final String SETTING_SAMPLE_CONFIG = "sampleConfigSetting";
 	private static final String SETTING_SWAGGER_BASE_PATH = "swaggerBasePath";
+	private static final String SETTING_SAMPLE_REPOSITORY = "sampleRepository";
 	
 	@NotEmpty
 	private String sampleConfigSetting;
 	
 	private String swaggerBasePath;
+	
+	private String sampleRepository;
 	
 	public SampleConfiguration() {
 		super(BASE_PATH);
@@ -57,4 +60,20 @@ public class SampleConfiguration extends EtcdConfiguration {
 		this.swaggerBasePath = setting;
 	}
 	
+	@JsonProperty
+	public String getSampleRepository() {
+		String result = getSetting(SETTING_SAMPLE_REPOSITORY);
+		if (result == null) {
+			result = sampleRepository;
+		} else {
+			sampleRepository = result;
+		}
+		return result;
+	}
+
+	@JsonProperty
+	public void setSampleRepository(String setting) {
+		this.sampleRepository = setting;
+	}
+
 }
