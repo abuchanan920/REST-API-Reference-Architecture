8075fac6ffd438a612f05792342f15a67cc4111a
Andrew W. Buchanan <buchanan@difference.com>
2014-08-11 18:01:47 -0400

    Increased the timeout to make sure the cluster stabilizes before we try using it

--- coreos/start.sh
+++ coreos/start.sh
@@ -6,7 +6,7 @@ vagrant up
 # Vagrant can't set docker containers to autostart in CoreOS yet, so manually reprovision...
 vagrant provision docker-registry
 # Set the config value to an initial value
-sleep 10
+sleep 20
 etcdctl --peers 172.17.8.101:4001 set /restreference/swaggerBasePath "https://rest"
 # Add the key to the VMs so some of the command line tools work
 ssh-add ~/.vagrant.d/insecure_private_key
