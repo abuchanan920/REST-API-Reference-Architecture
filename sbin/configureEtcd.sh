3917cc1e8e947aca5a39cbd64945712ec709f2fb
Andrew W. Buchanan <buchanan@difference.com>
2014-08-12 15:40:53 -0400

    Clear out sample repository data when reconfiguring etcd

--- sbin/configureEtcd.sh
+++ sbin/configureEtcd.sh
@@ -4,3 +4,4 @@ etcdctl set /restreference/swaggerBasePath "https://rest"
 etcdctl set /config/haproxy/restreference-api/HAPROXY_BACKEND_SERVICE "restreference-api"
 etcdctl set /config/haproxy/restreference-api/HAPROXY_DEFAULTS_OPTION_HTTPCHK "HEAD \/healthcheck"
 etcdctl set /config/haproxy/restreference-api/HAPROXY_HTTPCHK_PORT "8081"
+etcdctl rm --recursive /data/restreference
