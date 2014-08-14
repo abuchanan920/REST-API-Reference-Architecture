f93a656626afec03588b6546ec3b8363928d549c
Andrew W. Buchanan <buchanan@difference.com>
2014-08-14 15:32:52 -0400

    Build fleet unit files from templates

---
+++ fleet/createUnitFiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CONFCHANNEL=$1
+
+rm -rf units
+mkdir units
+
+function processTemplate {
+	TEMPLATE=$1
+	COUNT=$2
+	for i in `seq 1 $COUNT`; do
+		UNITFILE=units/$TEMPLATE.$i.service
+		cp templates/$TEMPLATE.template $UNITFILE
+		sed -i "" -e "s|<CONFCHANNEL>|$CONFCHANNEL|g" $UNITFILE
+		sed -i "" -e "s|<UNIT-ID>|$i|g" $UNITFILE
+		sed -i "" -e "s|<DOCKER-REGISTRY>|$DOCKER_REGISTRY|g" $UNITFILE
+	done
+}
+
+processTemplate logstash 1
+processTemplate logstash-forwarder 3
+processTemplate restreference-api 2
+processTemplate restreference-api-discovery 2
+processTemplate restreference-api-loadbalancer 1
